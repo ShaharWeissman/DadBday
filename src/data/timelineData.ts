91a056fb464db8bef40602901b3c644beea3ff6c
shahar-weissman <shaharweissman89@gmail.com>
2025-04-26 17:41:46 +0300

    timeline create

---
+++ src/data/timelineData.ts
@@ -0,0 +1,22 @@
+import { TimelineItem } from "@/types/TimelineItem";
+
+const timelineData: TimelineItem[] = [
+  {
+    yearRange: "1955 - 1966",
+    image: "/images/baby.jpg",
+    text: "Dad was born, learned how to giggle, and began his path to legendary jokes!",
+  },
+  {
+    yearRange: "1967 - 1975",
+    image: "/images/kid.jpg",
+    text: "Early childhood fun, possibly discovered his love for BBQ and a taste for pranks!",
+  },
+  {
+    yearRange: "1976 - 1985",
+    image: "/images/kid2.jpg",
+    text: "Teen years: honed his comedic timing and burned some steaks along the way!",
+  },
+  // Add more items if you want
+];
+
+export default timelineData;
---
+++ src/types/TimelineItem.tsx
@@ -0,0 +1,5 @@
+export interface TimelineItem {
+  yearRange: string;
+  image: string;
+  text: string;
+}
